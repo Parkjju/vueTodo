05a1322a6fdc820699d3d8e42db55d33b24e006e
Parkjju <rudwns3927@gmail.com>
2021-09-30 03:39:09 +0900

    Todo 구현 완료

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in todoItems" :key="item.item" class="shadow">
+        <i
+          class="far fa-check-square checkBtn"
+          v-bind:class="{ checkBtnCompleted: item.completed }"
+          v-on:click="toggleComplete(item)"
+        ></i>
+        <span v-bind:class="{ textCompleted: item.completed }">
+          {{ item.item }}
+        </span>
+        <span class="removeBtn" v-on:click="removeTodo(item, index)">
+          <i class="fas fa-trash"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+    localStorage.getItem(this.todoItems);
+  },
+  methods: {
+    removeTodo: function(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(item) {
+      item.completed = !item.completed;
+      localStorage.removeItem(item.item);
+      localStorage.setItem(item.item, JSON.stringify(item));
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
